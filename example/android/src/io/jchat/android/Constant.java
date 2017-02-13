e014ab6bf3e6f09ebedace9c130d7d767365e19e
KenChoi <caiyaoguan@caiyaoguandeMacBook-Pro.local>
2017-02-13 10:01:49 +0800

    update demo

--- example/android/src/io/jchat/android/Constant.java
+++
@@ -1,9 +0,0 @@
-package io.jchat.android;
-
-public class Constant {
-
-    public static final String PICTURE_DIR = "/pictures/";
-    public static final int REQUEST_CODE_TAKE_PHOTO = 4;
-    public static final int REQUEST_CODE_SELECT_PICTURE = 6;
-
-}
