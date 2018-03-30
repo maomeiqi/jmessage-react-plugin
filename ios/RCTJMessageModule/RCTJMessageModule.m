cf42103385193ba7b1d84c7808ef755610da6f98
huangminlinux <380108184@qq.com>
2018-03-30 16:13:13 +0800

    add githistorymessage add isDecend params

--- ios/RCTJMessageModule/RCTJMessageModule.m
+++ ios/RCTJMessageModule/RCTJMessageModule.m
@@ -821,6 +821,12 @@ RCT_EXPORT_METHOD(getHistoryMessages:(NSDictionary *)param
     return;
   }
   
+  BOOL isDecend = false;
+  if (param[@"isDecend"]) {
+    NSNumber *number = param[@"isDecend"];
+    isDecend = [number boolValue];
+  }
+  
   [self getConversationWithDictionary:param callback:^(JMSGConversation *conversation, NSError *error) {
     if (error) {
       failCallback(@[[error errorToDictionary]]);
@@ -832,10 +838,14 @@ RCT_EXPORT_METHOD(getHistoryMessages:(NSDictionary *)param
       JMSGMessage *message = obj;
       return [message messageToDictionary];
     }];
-
+    
     if (!messageArr) {
       successCallback(@[@[]]);
       return;
+    }
+    
+    if (isDecend) {
+      messageDicArr = [[messageDicArr reverseObjectEnumerator] allObjects];
     }
     
     successCallback(@[messageDicArr]);
